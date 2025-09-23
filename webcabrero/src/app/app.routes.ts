e71566dfbd93fc38d5a8b5c9b6f940b9cb3a3598
NicolasM <mrzdude1@gmail.com>
2025-09-23 09:03:25 -0300

    pagina home

--- webcabrero/src/app/app.routes.ts
+++ webcabrero/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-import { NewsComponent } from './features/news/news';
 import { Cabrero } from './pages/cabrero/cabrero';
 import { Alcalde } from './pages/alcalde/alcalde';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-  { path: '', component: NewsComponent },
+  { path: '', component: Home },
   { path: 'cabrero', component: Cabrero},
   { path: 'alcalde', component: Alcalde},
-  // aquí podrás ir agregando más rutas (noticias, contacto, etc.)
   { path: '**', redirectTo: '' }
 ];
--- webcabrero/src/app/pages/home/home.ts
+++ webcabrero/src/app/pages/home/home.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+
+type Noticia = {
+  id: number;
+  titulo: string;
+  bajada: string;
+  imagen: string;
+  fecha: string; // ISO
+};
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, DatePipe],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  noticias: Noticia[] = [
+    {
+      id: 1,
+      titulo: 'Sembramos trigo para la trilla',
+      bajada: 'Vecinos y vecinas se sumaron a la siembra comunitaria.',
+      imagen: 'https://www.cabrero.cl/images/250831-siembra-trigox.jpg',
+      fecha: '2025-09-10',
+    },
+    {
+      id: 2,
+      titulo: 'Certificamos tres nuevos cursos',
+      bajada: 'Más de 60 personas finalizaron su capacitación.',
+      imagen: 'https://www.cabrero.cl/images/w250828-tres-cursos-PMJH.jpg',
+      fecha: '2025-09-05',
+    },
+    {
+      id: 3,
+      titulo: 'Papelucho en Cabrero',
+      bajada: 'Una función llena de risas y emoción para toda la familia.',
+      imagen: 'https://www.cabrero.cl/images/W250829-PAPELUCHO.jpg',
+      fecha: '2025-08-28',
+    },
+    {
+      id: 4,
+      titulo: 'Segundo listado oficial COSOC',
+      bajada: 'Se publica el segundo listado del consejo de la sociedad civil.',
+      imagen: 'https://www.cabrero.cl/images/BANNER-COSOC.png',
+      fecha: '2025-08-21',
+    },
+  ];
 }
